37a563343a141bf91472ebaf6466e952c5ca3b74
mayur1008 <mayur.awati901@gmail.com>
2025-11-15 08:22:42 +0530

    UI

--- src/services/RoleCompetencyService.js
+++ src/services/RoleCompetencyService.js
@@ -1,10 +1,23 @@
 import axiosConfig from '../util/axios';
+import { store } from '../store/index';
 
 const RoleCompetencyService = {
   // Get all role-competency mappings
   async getRoleCompetencyMappings() {
     try {
-      const response = await axiosConfig().get('CompetencyMatrix/GetEmployeeRoleCompetencyList');
+      // Get user info from Redux store
+      const state = store.getState();
+      const institutionId = state.user?.userInfo?.institutionId;
+      
+      if (!institutionId) {
+        console.error('Institution ID not found in user info');
+        throw new Error('User institution information not available');
+      }
+      
+      const response = await axiosConfig().post(
+        'CompetencyMatrix/GetEmployeeRoleCompetencyList',
+        { institutionId }
+      );
       return response.data?.data || [];
     } catch (error) {
       console.error('Error fetching role-competency mappings:', error);
